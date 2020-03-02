6149057b0a3b618d05445294468a6a15b23fdbfe
Teerth Patel <teerth1205@gmail.com>
2020-03-02 04:07:40 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+has_many :characters
+has_many :shows, through: :characters
+
+def full_name 
+  "#{self.first_name} #{self.last_name}" 
+end 
+
+def list_roles
+  arr_of_char = self.characters.map {|char| char.name }
+  arr_of_shows = self.shows.map {|sh| sh.name }
+ role = arr_of_char.concat arr_of_shows
+ role.join(" - ")
+end 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters 
+  belongs_to :network
+  def actors_list
+    self.actors.map do |n|
+      "#{n.first_name} #{n.last_name}" 
+    end 
+    
+  end 
 end
